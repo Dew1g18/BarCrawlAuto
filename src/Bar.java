bff0e2bf87f104fbd7673c0012a3aebb7d5f7d2d
Dave <dew1g18@soton.ac.uk>
2019-11-25 16:40:59 +0000

    initial commit, create git

---
+++ src/Bar.java
@@ -0,0 +1,11 @@
+public class Bar {
+    String name;
+    String postcode;
+    String uniqueID;
+
+    public Bar(String name, String postcode, String uniqueID) {
+        this.name = name;
+        this.postcode = postcode;
+        this.uniqueID = uniqueID;
+    }
+}
