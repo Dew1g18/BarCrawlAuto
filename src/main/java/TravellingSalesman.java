facd73cc27aac4a5eef9d1385987fd3e372d8e68
Dave <dew1g18@soton.ac.uk>
2019-12-04 22:25:15 +0000

    placeholder class TravellingSalesman created

--- src/main/java/TravellingSalesman.java
+++ src/main/java/TravellingSalesman.java
@@ -1,2 +1,5 @@
 public class TravellingSalesman {
+
+    //Todo implement travelling salesman, probably from google.ortools, not sure how to use maven with it though
+    //will work it out tomorrow
 }
