2ee9d1ddd171906fc50a37f576a69207ffc643ba
Dave <dew1g18@soton.ac.uk>
2019-12-04 14:27:50 +0000

    Added some todos and just looking at a new api for verifying postcodes and getting lat longs for them. Doing this even though the google api will be getting the info for all the bars

--- src/Backend.java
+++ src/Backend.java
@@ -1,15 +1,49 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+
 
 public class Backend implements BackendInterface {
-    private HashMap<String, Double> begin;
+    private Map<String, Double> begin;
     private int numberOfStops;
 
-    public Backend(HashMap begin, int numberOfStops){
-        setParams(begin, numberOfStops);
+    public Backend(String begin, int numberOfStops){
+        Map<String, Double> latLongBegin = latLongFromPostcode(begin);
+//TODO Change string postcode to lat long
+//        setParams(begin, numberOfStops);
 
+    }
+
+    public Backend(Map begin, int numberOfStops){
+        setParams(begin, numberOfStops);
+    }
+
+    Map<String, Double> latLongFromPostcode(String postcode){
+        try {
+
+            URL url = new URL("api.postcodes.io/postcodes/"+postcode);
+            //todo CHECK that a postcode works with the same API once you know you're using the api correctly
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            String info = reader.readLine();
+            System.out.println(info);
+            Map<String, Double> latLong = new HashMap<>(){{
+                put("latitude",0.0);
+                put("longitude",0.0);
+            }};
+            //todo implement getting lat long from json
+
+            return latLong;
+        }catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
@@ -23,21 +57,27 @@ public class Backend implements BackendInterface {
     }
 
     @Override
-    public void setParams(HashMap begin, int numberOfStops) {
+    public void setParams(Map<String, Double> begin, int numberOfStops) {
         this.begin=begin;
         this.numberOfStops=numberOfStops;
     }
 
-    private void getGooglesInfo(Double lat, Double lon, int numberOfStops){
+    private boolean getGooglesInfo(Double lat, Double lon, int numberOfStops){
         String params = "rankby=distance&type=bar&fields=formatted_address&";
+
         try {
+            //todo finish forming the http request correctly
+            URL url = new URL("https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + params + "key=AIzaSyDXKLWHJQdqzVI1agSREbzr4AuoBKyUeuE");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            String info = reader.readLine();
+            //todo by this point reading the json should be trivial so use that to read bar information and create bars.
 
-            String location = "location="+Double.toString(lat)+","+Double.toString(lon)+"&";
-
-            URL url = new URL("https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + params + location + "key=AIzaSyDXKLWHJQdqzVI1agSREbzr4AuoBKyUeuE");
-
-        }catch(MalformedURLException e){
+            return true;
+        }catch(Exception e){
             e.printStackTrace();
+            return false;
         }
     }
 
@@ -46,7 +86,7 @@ public class Backend implements BackendInterface {
     }
 
     @Override
-    public HashMap<Bar, Number> getBarsOrderMap() {
+    public Map<Bar, Number> getBarsOrderMap() {
         return null;
     }
 }
